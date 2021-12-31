302044effd5425651b1228e51c6dae465f3ec7bf
hm243695czl <1224311695@qq.com>
2021-12-31 13:53:26 +0800

    完善分页bug

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,17 +1,29 @@
-import React, { Component } from 'react';
-import { ConfigProvider } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { ConfigProvider, Row, Pagination } from 'antd';
 import zhCN from 'antd/es/locale/zh_CN';
-class Pagination extends Component {
-    render() {
-        return (
-            <ConfigProvider locale={zhCN}>
-                {
-                    this.props.children
-                }
-            </ConfigProvider>
-        )
-    }
-
+const pageSizeOptions = [3, 5, 10, 20, 50, 100];
+export default function MyPagination({
+    total,
+    page,
+    changePage
+}) {
+    const [pageSize, setPageSize] = useState(pageSizeOptions[0]);
+    const change = (pageIndex, pageSize) => {
+        setPageSize(pageSize);
+        changePage({ first: pageIndex, rows: pageSize});
+    };
+    return (
+        <ConfigProvider locale={zhCN}>
+            <Row justify='end' className='pagination-container'>
+                <Pagination
+                    showSizeChanger
+                    showTotal={total => `共${total}条`}
+                    onChange={change}
+                    current={page}
+                    total={total}
+                    pageSizeOptions={pageSizeOptions}
+                />
+            </Row>
+        </ConfigProvider>
+    )
 }
-
-export default Pagination
